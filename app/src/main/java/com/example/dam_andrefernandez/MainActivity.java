92df02addeb39b9dbb0eb5bab39051848e6e8fe7
Andre Fernandez <2123110296@untels.edu.pe>
2024-10-09 15:28:22 -0500

    Botones y texto

--- app/src/main/java/com/example/dam_andrefernandez/MainActivity.java
+++ app/src/main/java/com/example/dam_andrefernandez/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.dam_andrefernandez;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Ajuste de las barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Obtener el ImageButton y configurar un click listener
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mostrar un Toast cuando se haga clic en el botón
+                Toast.makeText(MainActivity.this, "Se envió sus datos", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
